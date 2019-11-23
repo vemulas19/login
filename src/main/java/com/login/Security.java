b876f1874f909b7b3b50af24ea1e1cdb213049b6
kamalakar <kamalakar@LAPTOP-HK1FK6PA>
2019-11-23 17:29:22 +0530

    implemented sum method for calculation.

--- src/main/java/com/login/Security.java
+++ src/main/java/com/login/Security.java
@@ -5,7 +5,15 @@ public class Security {
 		System.out.println("hai ");
 		int a = 10;
 		int b=20;
-		System.out.println(a+b);
+		Security s = new Security();
+		int total = s.sum(a, b);
+		System.out.println("The sum is : " + total);
 		
+	}
+	
+	public int sum(int a, int b) {
+		int c = a+b;
+		System.out.println("Summation calculation done!!");
+		return c;
 	}
 }
