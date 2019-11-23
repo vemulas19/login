d411f683246263f746121666a5a27d1bb8bbce3b
Sai <Sai@192.168.0.4>
2019-11-23 13:56:02 +0530

    calculation is done.

--- src/main/java/com/login/Security.java
+++ src/main/java/com/login/Security.java
@@ -3,6 +3,9 @@ package com.login;
 public class Security {
 	public static void main(String[] args) {
 		System.out.println("hai ");
-		int a = 10
+		int a = 10;
+		int b=20;
+		System.out.println(a+b);
+		
 	}
 }
