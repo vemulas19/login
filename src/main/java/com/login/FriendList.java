ff67174850ce1e96e298c3508f340c9beecea850
chari455 <58098693+chari455@users.noreply.github.com>
2019-11-23 09:56:46 +0530

    Update FriendList.java

--- src/main/java/com/login/FriendList.java
+++ src/main/java/com/login/FriendList.java
@@ -5,6 +5,7 @@ public class FriendList {
 	public static void main(String[] args) {
 		System.out.println("frien list implemented!!");
 		System.out.println("frienD list implemented!!");
+		System.out.println("frienDs list implemented!!");
 		
 		
 	}
