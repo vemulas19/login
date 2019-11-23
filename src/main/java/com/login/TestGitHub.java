309159e6b5a773931ad0cf048e94e481ef1d0b8f
manasa <manasa@manasa-PC>
2019-11-23 21:33:51 +0530

    substraction is done!!

--- src/main/java/com/login/TestGitHub.java
+++ src/main/java/com/login/TestGitHub.java
@@ -3,6 +3,14 @@ package com.login;
 public class TestGitHub {
 	public static void main(String[] args) {
 		System.out.println("Done !!");
+		int sub=subStraction(500, 250);
+		System.out.println("substraction of given numbers:"+ sub);
+	}
+	
+	public  static int subStraction(int num1, int num2){
+		int c=num1-num2;
+		System.out.println("substraction is done");
+		return c;
 	}
 
 }
