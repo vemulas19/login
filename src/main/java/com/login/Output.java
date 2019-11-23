4933c5018153d896ec66590cd42979d485601d0c
usar <usar@DESKTOP-BMP6C86>
2019-11-23 09:59:35 +0530

    done

--- src/main/java/com/login/Output.java
+++ src/main/java/com/login/Output.java
@@ -3,7 +3,7 @@ package com.login;
 public class Output {
 	public static void main(String[] args) {
 		System.out.println("GIT");
-		System.out.println("GIT");
+		System.out.println("GIT ");
 	}
 
 }
