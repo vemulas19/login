889d03a0f08165a5e4dd1559471b10fa737a35d3
narmetaa <47554374+narmetaa@users.noreply.github.com>
2019-11-23 09:55:00 +0530

    Update FriendList.java

--- src/main/java/com/login/FriendList.java
+++ src/main/java/com/login/FriendList.java
@@ -4,5 +4,8 @@ public class FriendList {
 
 	public static void main(String[] args) {
 		System.out.println("frien list implemented!!");
+		System.out.println("frienD list implemented!!");
+		
+		
 	}
 }
