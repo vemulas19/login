c94aef586586ac288aecf9852b8e5ee7567e9279
Raashi Chowdary <Raashi Chowdary@lenovo>
2019-11-23 21:33:39 +0530

    addition implemented

--- src/main/java/com/login/Notifications.java
+++ src/main/java/com/login/Notifications.java
@@ -5,6 +5,22 @@ public class Notifications {
 	public static void main(String[] args) {
 		
 		System.out.println("getting notifications");
+		
+		int sum = addition(10, 20);
+		System.out.println("addition of entered two numbers is:" +sum);
+		
+		
 	}
+	
+	public static Integer addition(int num1, int num2) {
+		int num3= num1+num2;
+		
+		System.out.println("addition is done");
+		return num3;
+		
+		
+	}
+		
+		
 
 }
