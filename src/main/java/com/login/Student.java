e3ec6fad6b069a072f6e73fbc333e7147253583a
kamalakar <kamalakar@LAPTOP-HK1FK6PA>
2019-11-23 18:00:37 +0530

    multiplication is done

--- src/main/java/com/login/Student.java
+++ src/main/java/com/login/Student.java
@@ -3,5 +3,16 @@ package com.login;
 public class Student {
 public static void main(String[] args) {
 	System.out.println("student details");
+	int mul = mulitiplication();
+	System.out.println("Multiplication is : " + mul);
 }
+
+	public static int mulitiplication() {
+		
+		int a=10;
+		int b=20;
+		int c=a*b;
+		System.out.println("Done with calcuation");
+		return c;
+	}
 }
