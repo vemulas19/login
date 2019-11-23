f281e4b582e6fb0486649f9a9c85b28421c0dc5a
Moumika <Moumika@lenovo>
2019-11-23 22:13:08 +0530

    execute pattern program

--- src/main/java/com/login/UploadPage.java
+++ src/main/java/com/login/UploadPage.java
@@ -3,6 +3,19 @@ package com.login;
 public class UploadPage {
 	public static void main(String[] args) {
 		System.out.println("upload files --!!saa");
+	
+		
+		for(int i=0;i<=5;i++)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+		 System.out.println();
+		
+		}
+		
 	}
+
 
 }
