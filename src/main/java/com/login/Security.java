55e3055b6e8d40586f97b42db7867a60af36c431
kamalakar <kamalakar@LAPTOP-HK1FK6PA>
2019-11-23 17:35:26 +0530

    worked on review comments

--- src/main/java/com/login/Security.java
+++ src/main/java/com/login/Security.java
@@ -3,10 +3,10 @@ package com.login;
 public class Security {
 	public static void main(String[] args) {
 		System.out.println("hai ");
-		int a = 10;
-		int b=20;
-		Security s = new Security();
-		int total = s.sum(a, b);
+		int number1 = 10;
+		int number2=20;
+		Security security = new Security();
+		int total = security.sum(number1, number2);
 		System.out.println("The sum is : " + total);
 		
 	}
